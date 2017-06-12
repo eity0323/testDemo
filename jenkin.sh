a43de450f0a6d37a6e684c81b7a8df55129b2765
eity0323 <312726446@qq.com>
2017-06-12 16:48:56 +0800

    jenkin配置调整

--- jenkin.sh
+++ jenkin.sh
@@ -1,18 +1,20 @@
 mvn clean package -Dmaven.test.skip=true
 mv target/*.war ROOT.war
-REGISTRY_HOST=registry.pass:443
+REGISTRY_HOST=registry.paas:443
 USER_NAME=eity0323
-CI_BUILD REF NAME=master
+CI_BUILD_REF_NAME=master
 IMAGE=testdemo
 buildtime=$(date +%Y%m%d%H%M%S)
  docker login -u $USER_NAME -p Eity0323 -e kevin@qq.com $REGISTRY_HOST
- docker build -t registry.pass:443/${USER_NAME}/${IMAGE}:{buildtime} -f Dockerfile .
+ docker build -t registry.paas:443/${USER_NAME}/${IMAGE}:${buildtime} -f Dockerfile .
  docker login -u $USER_NAME -p Eity0323 -e kevin@qq.com $REGISTRY_HOST
- docker push registry.paas:443/${USER_NAME}/${IMAGE}:{buildtime}
-result='curl -X POST \ -d "registry=${REGISTRY_HOST}&userName=$USER_NAME&image=$IMAGE&tag=${buildtime}&startPort=8080&desout=180000&start_timeout=120000&stop_timeout=120000&destory_timeout=120000" \ http://172.16.49.81:5081/svn/ws/registry/registResourceFromCI'
+ docker push registry.paas:443/${USER_NAME}/${IMAGE}:${buildtime}
+result=`curl -X POST \
+ -d "registry=${REGISTRY_HOST}&userName=$USER_NAME&image=$IMAGE&tag=${buildtime}&startPort=8080&description_resource=syh_docker_run&&description=v1.0000&deploy_timeout=180000&start_timeout=120000&stop_timeout=120000&destroy_timeout=120000" \
+http://172.16.49.81:5081/svn/ws/registry/registResourceFromCI`
 echo $result
-if [ "$result" == '{"result":true}'];then
-echo success
+if [ "$result" == '{"result":true }' ] ; then
+	echo success
 else
-exit 1
-fi 
+	exit 1
+fi
